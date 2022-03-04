f3d3ba7779e6b7d4091c92759035b9fe8913b8b4
Alexandr L <sasha.lebedenko.2012@gmail.com>
2022-03-04 21:53:13 +0300

    set links and scrolling

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+function scrolling() {
+  const anchors = document.querySelectorAll('a.scroll-to')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href')
+      
+      document.querySelector(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+}
+
+function render() {
+  scrolling();
+}
+
+render();
